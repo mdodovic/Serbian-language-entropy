899fc251b59d67e4de7f5ff47b4884b40f9c8463
Matija Dodovic <matija.dodovic@gmail.com>
2021-03-16 23:21:26 +0100

    H0 calculation added

--- entropy.py
+++ entropy.py
@@ -1,10 +1,16 @@
 import numpy as np
 import math as math
-text_file = open("file.txt","r");
+
+text_file = open("second_example.txt","r");
 
 characters = {}
+character_frequency = {}
+doublets_frequency = {}
+number_of_characters = 0
+
 for line in text_file:
     for character in line:
+        number_of_characters += 1
         if str(character) in characters:
             characters[str(character)] += 1
         else:
@@ -12,32 +18,39 @@ for line in text_file:
             
 text_file.close()
 
+for character, number_of_repetitions in characters.items():
+    character_frequency[character] = number_of_repetitions / number_of_characters
+
 q = len(characters)
 print("Number of different elements: ", q)
 
+# calculate the maximal entropy of the source without any statistical code implemented
+Hmax = math.log2(q)
+print("Hmax: ", Hmax) 
 
 
-character_frequency = {}
 for character, number_of_repetitions in characters.items():
-    character_frequency[character] = number_of_repetitions / q 
-
-for character, number_of_repetitions in characters.items():
-    print(character,"(",str(type(character)),") : ", number_of_repetitions)
-
-
+    if character != '\n':   
+        print(character," : ", number_of_repetitions)
+    else:
+        print("\\n : ", number_of_repetitions)
+ 
 for character, frequency in character_frequency.items():
-    print(character,"(",str(type(character)),") : ", frequency)
+    if character != '\n':   
+        print(character," : ", frequency)
+    else:
+        print("\\n : ", frequency)
+ 
+
+
+
+# calculate the entropy of the source without the memory
 
 H0 = 0
-
 for character, frequency in character_frequency.items():
     H0 += frequency * math.log2(frequency)
 
 H0 = -H0
-
-Hmax = math.log2(q)
-print("Hmax: ", Hmax) 
-
 print("H0: ", H0) 
 
 
